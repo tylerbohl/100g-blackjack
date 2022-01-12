9632b9c199913a177b87bc13010c4929d554b406
hungrybeagle <cyang@deltalearns.ca>
2022-01-12 14:23:03 -0800

    Update x04_dealer.py

--- x04_dealer.py
+++ x04_dealer.py
@@ -27,8 +27,8 @@ def dealer(deck):
   return [ dealer , score , deck ]
 
 def main():
-  deck = ['3C', '3S', '8S', '3D', '4D', 'AC', '9H', 'QC', 'TD', 'TH', '8H', '8D', '7C', 'TS', '7D', 'AD', 'QD', 'KC', '6H', 'JH', 'KH', 'QS', '6C', '4H', '7H', '5S', '2S', 'AS', 'AH', '5C', '2D', '2H', '6D', 'TC', '4C', 'JS', 'JC', 'KD', '2C', '4S', '3H', '5H', '7S', 'KS', '5D', 'QH', '6S', '8C', '9D', 'JD', '9S', '9C']
+  deck = ['3C', '3S', '8S', '3D', 'AC', '9H', 'QC', 'TD', 'TH', '8H', '8D', '7C', 'TS', '7D', 'AD', 'QD', 'KC', '6H', 'JH', 'KH', 'QS', '6C', '4H', '7H', '5S', '2S', 'AS', 'AH', '5C', '2D', '2H', '6D', 'TC', '4C', 'JS', 'JC', 'KD', '2C', '4S', '3H', '5H', '7S', 'KS', '5D', 'QH', '6S', '8C', '9D', 'JD', '9S', '9C', '4D']
   run1 = dealer(deck)
-  assert dealer(deck) == [['3C', '3S', '8S', '3D', '4D'], 21, run1[2] ]
+  assert dealer(deck) == [['3C', '3S', '8S', '3D'], 17, run1[2] ]
   run2 = dealer( run1[2] )
   assert dealer(run1[2]) == [['AC', '9H'], 20, run2[2] )
